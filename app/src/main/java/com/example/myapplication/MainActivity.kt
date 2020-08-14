862b3eedc530ca7f23342b1a0a23d749763b1112
v_xhangxie <v_xhangxie@tencent.com>
2020-08-14 15:10:46 +0800

    二〇二〇年八月十四日 15:10:44

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.http.NetConfig
 import com.example.http.RequestService
 import com.example.http.RetrofitUtils
+import kotlinx.android.synthetic.main.activity_main.*
 import model.DataBean
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,9 +17,14 @@ class MainActivity : AppCompatActivity() {
     val TAG = "xxxxxxH"
     val retrofit = RetrofitUtils.get().retrofit
     val service = retrofit.create(RequestService::class.java)
+    var dataBean : DataBean? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        StatusBarUtil.setTranslucent(this)
+        img.setBackgroundResource(R.mipmap.bg)
+        collapsing.setExpandedTitleColor(Color.WHITE)
+        collapsing.setCollapsedTitleTextColor(Color.WHITE)
         service.getDatas(NetConfig().VERSION_CODE, NetConfig().APP_ID, NetConfig().APP_SERCERT)
             .enqueue(object : Callback<DataBean> {
                 override fun onFailure(call: retrofit2.Call<DataBean>, t: Throwable) {
@@ -29,6 +36,7 @@ class MainActivity : AppCompatActivity() {
                     response: Response<DataBean>
                 ) {
                     Log.i(TAG, "onResponse")
+//                    city_name.text = response.body()!!.city
                 }
             })
     }
